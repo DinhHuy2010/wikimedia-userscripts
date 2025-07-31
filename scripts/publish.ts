7c6d53634c34fee7ac6fd0cbd3c2792834215f49
DinhHuy2010 <huy04052010@outlook.com>
2025-07-31 23:22:22 +0700

    update publish.ts

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -2,6 +2,8 @@ import { Mwn } from "mwn";
 
 const WIKIMEDIA_USERNAME = Deno.env.get("WIKIMEDIA_USERNAME");
 const WIKIMEDIA_PASSWORD = Deno.env.get("WIKIMEDIA_PASSWORD");
+const USER_AGENT = `DinhHuy2010/wikimedia-userscripts Deno/${Deno.version.deno} (target to: User:${WIKIMEDIA_USERNAME}/global.js)`;
+const TARGET_PAGE = "User:DinhHuy2010/global.js";
 
 if (!WIKIMEDIA_USERNAME || !WIKIMEDIA_PASSWORD) {
     console.error(
@@ -15,10 +17,18 @@ async function main(): Promise<void> {
         apiUrl: "https://meta.wikimedia.org/w/api.php",
         username: WIKIMEDIA_USERNAME,
         password: WIKIMEDIA_PASSWORD,
-        userAgent: ""
+        userAgent: USER_AGENT,
     })
+    console.log("Loading 'wikimedia-userscript-global.esbuilt.js'...");
+    const script = await Deno.readTextFile("wikimedia-userscript-global.esbuilt.js");
+    const editsummary = "Update global.js from wikimedia-userscripts repository";
+    console.log("Updating global.js...");
+    const response = await wp.save(TARGET_PAGE, script, editsummary);
+    console.log("Update complete.");
+    console.log(`Result: ${response.result}`);
+    console.log(`Revision ID: ${response.newrevid}`);
 }
-
+    
 if (import.meta.main) {
     main();
 }
