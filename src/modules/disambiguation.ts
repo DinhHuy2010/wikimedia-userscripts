46260cc3043d676f98a06f4be87eea36b9c689bd
DinhHuy2010 <huy04052010@outlook.com>
2025-08-09 22:28:46 +0700

    refactor: enhance isPageDisambiguation function to accept namespace and improve error handling

--- src/modules/disambiguation.ts
+++ src/modules/disambiguation.ts
@@ -1,27 +1,36 @@
 import { DISAMBIGUATION_PAGE_API_QUERY } from "../constants.ts";
 import { setTabLabel } from "./tabs.ts";
-import { toContentNamespace } from "./utils.ts";
+import { log, toContentNamespace } from "./utils.ts";
 
-async function isPageDisambiguation(page: string): Promise<boolean> {
-    const titleobj = mw.Title.newFromText(page);
-    if (!titleobj) {
-        throw new Error(`Invalid page name: ${page}`);
-    }
+async function isPageDisambiguation(
+    title: string,
+    ns: number,
+): Promise<boolean> {
     const query = structuredClone(DISAMBIGUATION_PAGE_API_QUERY);
-    const contentNS = toContentNamespace(titleobj.getNamespaceId());
+    const contentNS = toContentNamespace(ns);
     const api = new mw.Api();
     query.titles = `${
         mw.config.get("wgFormattedNamespaces")[contentNS]
-    }:${titleobj.getMainText()}`;
+    }:${title}`;
     const data = await api.get(query);
-    const isDisambig =
-        (data.query.pages[0].pageprops.disambiguation || null) === "";
+    const isDisambig = data.query.pages[0].pageprops.disambiguation === "";
     return isDisambig;
 }
 
-export async function setDisambiguationLabel(): Promise<void> {
-    const isDisambig = await isPageDisambiguation(mw.config.get("wgPageName"));
-    if (isDisambig) {
-        setTabLabel("Disambiguation page");
+export function setDisambiguationLabel(): void {
+    async function updateTabForDisambiguation() {
+        const isDisambig = await isPageDisambiguation(
+            mw.config.get("wgTitle"),
+            mw.config.get("wgNamespaceNumber"),
+        );
+        if (isDisambig) {
+            log("Setting disambiguation label for the current tab...");
+            setTabLabel("Disambiguation page");
+        }
     }
+    mw.loader.using("mediawiki.api").then(
+        async () => {
+            await updateTabForDisambiguation();
+        },
+    );
 }
