31e27dfd97aeae7b2fa99dc04bce3dc26c6675b2
DinhHuy2010 <huy04052010@outlook.com>
2025-08-11 00:33:17 +0700

    Revert "feat: add hideEditButtonOnDiscussionPages utility and integrate it into options"
    This reverts commit 0b46ac6028d16e879753a6da033e0abfe5fbf684.

--- src/modules/utils.ts
+++ src/modules/utils.ts
@@ -2,7 +2,7 @@ import { ApiParseParams } from "../types.ts";
 import { SCRIPT_NAME } from "../constants.ts";
 import { dhoptions } from "../options.ts";
 
-export function isTalkNamespace(n: number): boolean {
+function isTalkNamespace(n: number): boolean {
     return n >= 0 && n % 2 === 1;
 }
 
