b0369af0f5194f000cabd287a846fce27e26dec9
DinhHuy2010 <huy04052010@outlook.com>
2025-08-15 22:50:36 +0700

    feat: refactor shouldHideEditButton logic and improve API parameter handling

--- src/modules/onpages.ts
+++ src/modules/onpages.ts
@@ -1,5 +1,13 @@
 import { NAMESPACE } from "../constants.ts";
-import { isTalkNamespace } from "../utils.ts";
+import { ApiQueryPagePropsParams } from "../types.ts";
+import { isTalkNamespace, toContentNamespace } from "../utils.ts";
+
+const BASE_PAGE_PROPS_API_PARAMS: ApiQueryPagePropsParams = {
+    "action": "query",
+    "format": "json",
+    "prop": "pageprops",
+    "formatversion": "2",
+};
 
 function hideEditButton(): void {
     $("#ca-edit").hide();
@@ -9,27 +17,40 @@ function hideViewSourceButton(): void {
     $("#ca-viewsource").hide();
 }
 
-function shouldHideEditButton(): "edit" | "viewsource" | null {
-    // MediaWiki talk does not have a "Add topic" button
-    if (isTalkNamespace(NAMESPACE) && NAMESPACE !== 9) {
-        return "edit";
+async function isNonTalkPageIsDiscussion(
+    title: string,
+    ns: number,
+): Promise<boolean> {
+    const p = structuredClone(BASE_PAGE_PROPS_API_PARAMS);
+    p.titles = `${
+        mw.config.get("wgFormattedNamespaces")[toContentNamespace(ns)]
+    }:${title}`;
+    p.ppprop = "newsectionlink";
+    const api = new mw.Api();
+    const response = await api.get(p);
+    return response.query?.pages[0].pageprops?.newsectionlink === "";
+}
+
+async function shouldHideEditButton(): Promise<"edit" | "viewsource" | null> {
+    const button = mw.config.get("wgIsProbablyEditable")
+        ? "edit"
+        : "viewsource";
+    if (isTalkNamespace(NAMESPACE)) {
+        return button;
     }
-    if (NAMESPACE == 8) {
-        // MediaWiki namespace
-        if (mw.config.get("wgIsProbablyEditable") === false) {
-            return "viewsource";
-        } else {
-            return "edit";
-        }
+    if (await isNonTalkPageIsDiscussion(mw.config.get("wgTitle"), NAMESPACE)) {
+        return button;
     }
     return null;
 }
 
 export function onPages(): void {
-    const buttonToHide = shouldHideEditButton();
-    if (buttonToHide === "edit") {
-        hideEditButton();
-    } else if (buttonToHide === "viewsource") {
-        hideViewSourceButton();
-    }
+    const buttonToHidePromise = shouldHideEditButton();
+    buttonToHidePromise.then((buttonToHide) => {
+        if (buttonToHide === "edit") {
+            hideEditButton();
+        } else if (buttonToHide === "viewsource") {
+            hideViewSourceButton();
+        }
+    });
 }
