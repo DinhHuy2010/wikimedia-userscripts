de4e7fe40c1bce6953d2aa7bddd0862851b94ee1
DinhHuy2010 <huy04052010@outlook.com>
2025-08-24 15:50:20 +0700

    feat: refactor build and publish scripts to utilize dynamic configuration and improve error handling

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,36 +1,44 @@
-import * as esbuild from "esbuild";
+import { analyzeMetafile, build, BuildResult } from "esbuild";
 
-const HEADER_JS = `/**
- * User:DinhHuy2010/global.js
- * This script is loaded on all Wikimedia projects.
- * @copyright 2025-present DinhHuy2010
- * @license "CC-BY-4.0 OR MIT"
- * @repository https://github.com/DinhHuy2010/wikimedia-userscripts
- * @builtOn ${new Date().toISOString()}
- * @esbuildVersion ${esbuild.version || "unknown"}
- */`;
-
-const HEADER_CSS = `/**
- * User:DinhHuy2010/global.css
- * This stylesheet is loaded on all Wikimedia projects.
- * @copyright 2025-present DinhHuy2010
- * @license "CC-BY-4.0 OR MIT"
- * @repository https://github.com/DinhHuy2010/wikimedia-userscripts
- * @builtOn ${new Date().toISOString()}
- * @esbuildVersion ${esbuild.version || "unknown"}
- */`;
-
-function main(): void {
-    esbuild.buildSync({
-        bundle: true,
-        platform: "browser",
-        entryPoints: ["./src/global.ts", "./src/global.css"],
-        outdir: "./build",
-        banner: { js: HEADER_JS, css: HEADER_CSS },
-        legalComments: "none",
-        target: ["es6"],
-    });
+async function loadConfig() {
+    const module = await import("../config.ts");
+    return module.config;
 }
+const conf = await loadConfig();
+
+function onBuildComplete(result: BuildResult): void {
+    if (result.errors.length > 0) {
+        console.error("Build failed with errors:");
+        result.errors.forEach((err) => console.error(err));
+    } else {
+        // Get output paths
+        console.log(`Build succeeded`);
+        if (result.metafile) {
+            console.log("Output files:");
+            for (const outputFile in result.metafile.outputs) {
+                console.log(` - ${outputFile}`);
+            }
+            console.log("esbuild metafile:");
+            analyzeMetafile(result.metafile).then((_) => (console.log(_)));
+        }
+    }
+}
+
+function buildBuildPromises(): Promise<void>[] {
+    const promises: Promise<void>[] = conf.buildTargets.map(
+        async (target) => {
+            const result = await build(target.options);
+            onBuildComplete(result);
+        },
+    );
+    return promises;
+}
+
+async function main(): Promise<void> {
+    const buildPromises = buildBuildPromises();
+    await Promise.all(buildPromises);
+}
+
 if (import.meta.main) {
-    main();
+    await main();
 }
